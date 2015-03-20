b8193941aeda381833d72fc1ae20a8b1cfc122d7
Miles Matthias <miles.matthias@gmail.com>
2015-03-19 21:00:44 -0600

    dry

--- lib/synapse_client/order.rb
+++ lib/synapse_client/order.rb
@@ -18,20 +18,7 @@ module SynapseClient
     attr_reader :fee
 
     def initialize(options = {})
-      @status        = options[:status]
-      @amount        = options[:amount]
-      @seller_email  = options[:seller_email]
-      @bank_pay      = options[:bank_pay]
-      @bank_id       = options[:bank_id]
-      @note          = options[:note]
-      @date_settled  = options[:date_settled]
-      @date          = options[:date]
-      @id            = options[:id]
-      @ticket_number = options[:ticket_number]
-      @resource_uri  = options[:resource_uri]
-      @account_type  = options[:account_type]
-      @fee           = options[:fee]
-      @seller_id     = options[:seller_id]     || options[:seller].delete("seller_id") rescue nil
+      update_attributes(options)
     end
 
     def self.all(params={})
@@ -68,7 +55,7 @@ module SynapseClient
       @resource_uri  = options[:resource_uri]
       @account_type  = options[:account_type]
       @fee           = options[:fee]
-      @seller_id     = options[:seller_id]     || options[:seller].delete("seller_id") rescue nil
+      @seller_id     = options[:seller_id] || options[:seller].delete("seller_id") rescue nil
     end
 
   end
